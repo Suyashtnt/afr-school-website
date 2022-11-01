cee6d910059c7fa20488cf9b995b9d1bc7ca2d73
Suyashtnt <suyashtnt@gmail.com>
2022-11-01 17:05:22 +0200

    switch to vercel edge functions for even more speed

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 import importAssets from 'svelte-preprocess-import-assets';
 
@@ -20,7 +20,9 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			edge: true
+		})
 	}
 };
 
