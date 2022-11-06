51e49045dbe4aa90a5f1f8642b430a1a0c164bad
Suyashtnt <suyashtnt@gmail.com>
2022-11-06 19:41:22 +0200

    Finish drugs.txt
    god this is awful

---
+++ src/lib/components/BtnForGroup.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import { BUTTON_GROUP } from './BtnGroup.svelte';
+	const { selectButton, values } = getContext(BUTTON_GROUP);
+
+	export let value: string;
+	export let active = true;
+	$: active = $values.includes(value);
+
+	let clazz = '';
+	export { clazz as class };
+</script>
+
+<button
+	on:click={() => selectButton(value)}
+	class:active
+	class="flex-grow {clazz} rounded-none border-none text-4xl"
+>
+	<slot />
+</button>
+
+<style>
+	.active {
+		background-color: red;
+	}
+</style>
